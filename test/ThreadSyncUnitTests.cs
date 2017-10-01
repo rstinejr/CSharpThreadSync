eb8058e09f373b5e999396b9d26962315a72c838
R.H. Stine <bob@waltonstine.net>
2017-10-01 11:25:45 -0400

    Add a couple of unit tests.

--- test/ThreadSyncUnitTests.cs
+++ test/ThreadSyncUnitTests.cs
@@ -1,17 +1,41 @@
 using System;
+using System.Threading;
 using Xunit;
-using waltonstine.demo.csharptemplate;
+using waltonstine.demo.csharpthreadsync;
 
-namespace waltonstine.demo.csharptemplate.unittests
+namespace waltonstine.demo.csharpthreadsync.unittests
 {
     public class MyCSharpTemplateUnitTests
     {
         [Fact]
-        public void CtorSmokeTest()
+        public void CheckWait()
         {
-            MyClass mine = new MyClass();
-            Assert.True(true);
+            /* 
+             * Show that wait works for letting the Thread finish initializaion.
+             */
+            ThreadWrapper tw = new ThreadWrapper();
+            tw.StartThread();
+            Assert.False(tw.InitIsDone);
+            tw.WaitForReady();
+            Assert.True(tw.InitIsDone);
+ 
         }
  
+        [Fact]
+        public void ManyWaits()
+        {
+            /*
+             * Show that multiple waits are benign.
+             */
+            ThreadWrapper tw = new ThreadWrapper();
+            tw.StartThread();
+
+            tw.WaitForReady();
+            tw.WaitForReady();
+            Assert.True(tw.InitIsDone);
+            Thread.Sleep(4);
+            Assert.True(tw.InitIsDone);
+            tw.WaitForReady();
+        }
     }
 }
